91677481d899652bc45cfe09879619a0ea712345
Zoxidjon <133236562+Zoxidali@users.noreply.github.com>
2026-04-20 15:14:23 +0500

    Add files via upload

---
+++ SumWithoutMassive.py
@@ -0,0 +1,33 @@
+# c=0
+# for i in range(10):
+#     a=float(input("sonni kiriting"))
+#     c=c+a
+
+# import random
+# s=0
+# for i in range (10):
+#     s=s+random.randit(1,100)
+#     s=s+random.uniform(1.0,100.0)
+# print(s)
+
+# g=1
+# n=4
+# for i in range(n):
+#     a=float(input("a:"))
+#     g=g*a
+# print(g**(1/n))
+
+# n=4
+# c=0
+# for i in range(n):
+#     a=float(input("sonni kiriting"))
+#     c=c+int(a)
+# print(int(c))
+
+# n=4
+# c=0
+# for i in range(n):
+#     a=float(input("sonni kiriting"))
+#     c=c*int(a)
+# print(int(c))
+
